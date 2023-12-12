049cfe3c2234fbab54cc3521abdddcb412ae1081
minhyeok2023 <149047754+minhyeok2023@users.noreply.github.com>
2023-12-12 19:19:38 +0900

    fnd

---
+++ fnd.h
@@ -0,0 +1,7 @@
+#ifndef _FND_H_
+#define _FND_H_
+
+int fndDisp(int num,int dotflag);
+
+#endif
+
---
+++ fndtest.c
@@ -0,0 +1,11 @@
+#include "fnd.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int number = 1234;
+	fndDisp(number,0b1010);
+	printf("%d",number);
+}
+
