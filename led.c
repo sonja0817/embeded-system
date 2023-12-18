c59ab826067a2abed46b96c954bcfdc87747e548
minhyeok2023 <149047754+minhyeok2023@users.noreply.github.com>
2023-12-18 12:59:51 +0900

    led 수정

--- led.c
+++ led.c
@@ -15,6 +15,18 @@ int ledOnOff(int ledNum,int onOff)
 	write (fd,&ledValue,4);
 }
 
+int ledOnOff_1(int Num){
+	int j=1;
+	if(Num==0)j=0;
+	else{
+		for(int i=1;i<Num;i++){
+			j=(j<<1)+1;
+		}
+	}
+	ledValue = j;
+	write(fd,&ledValue,4);
+}
+
 int ledLibInit(void)
 {
 	fd=open("/dev/periled",O_WRONLY);
--- led.h
+++ led.h
@@ -4,8 +4,8 @@ int ledLibInit(void);
 int ledOnOff(int ledNum,int onOff);
 int ledStatus(void);
 int ledLibExit(void);
-
+int ledOnOff_1(int Num);
 #define LED_DRIVER_NAME "/dev/periled"
 
-#endif _LED_H_
+#endif
 
