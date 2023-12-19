7bf52edff238f9b0a2b51f93d68f75762a5502d7
JiJunghyun00 <mju19jhj@gmail.com>
2023-12-19 13:00:02 +0900

    이미지4장받는api수정,현재시간등 model,routes수정

--- BackendServer/routes/posts.js
+++ BackendServer/routes/posts.js
@@ -14,15 +14,23 @@ router.route('/')
   })
   .post(async (req, res, next) => {
     try {
+        let year=new Date().getFullYear();
+        let month=new Date().getMonth()+1;
+        let date=new Date().toLocaleString('en-US',{timeZone:'Asia/Seoul',day:'numeric'});
+        let hours=new Date().toLocaleString('en-US',{timeZone:'Asia/Seoul',hour:'numeric'}).slice(0,-3);
+        let minutes=new Date().toLocaleString('en-US',{timeZone:'Asia/Seoul',minute:'numeric'});
+        let currentdate=year+'/'+month+'/'+date+' '+hours+':'+minutes;
       const posts = await Post.create({
         location: req.body.location,
         peopleNum:req.body.peopleNum,
         congestion:req.body.congestion,
-        date:req.body.date,
+        date:currentdate,
         image:req.body.image,
+        image1:req.body.image1,
+        image2:req.body.image2,
+        image3:req.body.image3
     });
-      console.log(posts);
-      res.status(201).json(posts);
+      res.status(200).json({message:'success');
     } catch (err) {
       console.error(err);
       next(err);
@@ -45,7 +53,7 @@ router.route('/:location')
   .get(async (req, res, next) => {
     try {
       const posts = await Post.findAll({
-        model: Post, // 관련된 모델을 지정
+        model: Post, 
          where: { location: req.params.location },
       });
       console.log(posts);
