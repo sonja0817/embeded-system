7eebc056a3476037557edec1d637023ca013ae31
sonja0817 <sms6101501@naver.com>
2023-12-20 15:17:11 +0900

    Update vision.cpp
    fix

--- vision.cpp
+++ vision.cpp
@@ -6,6 +6,11 @@
 using namespace cv;
 using namespace std;
 
+#define VIDEO
+
+
+#ifdef CAM
+
 int main(int argc, char* argv[])
 {
     Mat current_frame, cvt_frame;
@@ -30,7 +35,7 @@ int main(int argc, char* argv[])
     while (1)
     {
         cap >> current_frame;
-        
+        cvt_frame = current_frame.clone();
         //pMog2->apply(a, frame, 0);     //MOG2
         pMog2->operator()(current_frame, mog2, 0);
         kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
@@ -58,7 +63,7 @@ int main(int argc, char* argv[])
             }
         }
         
-        imshow("test", a);
+        imshow("test", cvt_frame);
 
         if (hist >= total_fix * 0.3 && hist < total_fix * 0.4)
         {
@@ -85,3 +90,84 @@ int main(int argc, char* argv[])
         waitKey(10);
     }
 }
+#endif
+
+#ifdef VIDEO
+int main(int argc, char* argv[])
+{
+    Mat current_frame, cvt_frame;
+    Mat kernel;
+    Mat mog2;
+    Mat a, b, c;
+    int level;
+    int hist = 0;
+    vector<vector<Point> > contours;
+    Ptr< BackgroundSubtractor> pMog2;
+    pMog2 = createBackgroundSubtractorMOG2();
+    
+    VideoCapture cap("river.mov");
+
+
+    cap >> current_frame;
+    int total_fix = current_frame.rows * current_frame.cols;
+
+
+    while (1)
+    {
+        cap >> current_frame;
+        cvt_frame = current_frame.clone();
+        
+        pMog2->apply(current_frame, mog2, 0);     //MOG2
+        kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+        morphologyEx(mog2, mog2, MORPH_ERODE, kernel);
+
+        imshow("ther", mog2);
+        findContours(mog2, contours,RETR_EXTERNAL, CHAIN_APPROX_NONE);
+        drawContours(cvt_frame, contours, -1, Scalar(0, 0, 255), -1);
+
+        /*for (int i = 0; i < contours.size(); i++)
+        {
+            Rect rect = boundingRect(contours[i]);
+            rectangle(a, rect, Scalar(0, 0, 255), -1);
+        }*/
+
+
+        for (int y = 0; y < a.rows; y++)
+        {
+            for (int x = 0; x < a.cols; x++)
+            {
+                if (a.at<Vec3b>(y, x)[2] == 255)
+                {
+                    hist++;
+                }
+            }
+        }
+        
+        imshow("test", cvt_frame);
+
+        if (hist >= total_fix * 0.3 && hist < total_fix * 0.4)
+        {
+            cout << "notice" << endl;
+            level = 2;
+        }
+        else if (hist >= total_fix * 0.4 && hist < total_fix * 0.5)
+        {
+            cout << "caution" << endl;
+            level = 4;
+        }
+        else if (hist >= total_fix * 0.6 && hist < total_fix * 0.7)
+        {
+            cout << "warning" << endl;
+            level = 6;
+        }
+        else if (hist >= total_fix * 0.7)
+        {
+            cout << "danger" << endl;
+            level = 8;
+        }
+
+        hist = 0;
+        waitKey(10);
+    }
+}
+#endif
