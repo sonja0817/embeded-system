d2fd57031ee503a010f0bf4f39304ed72358ad62
minhyeok2023 <149047754+minhyeok2023@users.noreply.github.com>
2023-12-20 15:34:08 +0900

    pr.c

--- pr.c
+++ pr.c
@@ -5,45 +5,52 @@
 #include "lcdtext.h"
 #include "projectheader.h"
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/msg.h>
 #include <string.h>
+#include "jpgViewer.h"
+
+pthread_t tid[2];
 
 int main(void){
+	pthread_create(&tid[0],NULL,&jpgViewer,NULL);
+	pthread_create(&tid[1],NULL,&clock_1,NULL);
 	buttonInit();
-	buzzerInit();
+	BuzzerInit();
 	ledLibInit();
-	int a=1234;
-	char b[10]= "hello";
 	int msgID = msgget (MESSAGE_ID, IPC_CREAT | 0666);
 	BUTTON_MSG_T recvdMsg;
 	int level;
 	int c=1;
+	int fp;
 
 	while(1)
 	{
 		if(msgrcv(msgID,&recvdMsg,sizeof(BUTTON_MSG_T)-sizeof(long int),0,IPC_NOWAIT)>=0)
 		{
 			if(recvdMsg.keyInput == 102){
-				//stop and transform
+				fp = open("/home/ecube/level.txt",O_RDWR|O_CREAT,0666);
+				read(fp,&level,4);
+				close(fp);
 				ledOnOff_1(level);
 				buzzer_density(level);
 				lcdtext_density(level);
-				fp = open("/home/ecube/check.txt",O_RDWR|O_CREAT,0666);
+				fp = open("/home/eucbe/check.txt",O_RDWR|O_CREAT,0666);
 				write(fp,&c,4);
-				close (fp);
-				fp = open("/home/eucbe/level.txt",O_RDWR|O_CREAT,0666);
-				write(fp,&level,4);
 				close (fp);
 			}//very left
 			else if(recvdMsg.keyInput == 158){
-				//play
+				break;
 			}
 		}
 		usleep(1000);
 	}
+	pthread_join(tid[0],NULL);
+	pthread_join(tid[1],NULL);
 	ledLibExit();
-	buzzerExit();
+	BuzzerExit();
 	buttonExit();
 
 }
