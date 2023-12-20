5619981463621d29c538386e5b4df620edd377a7
sonja0817 <sms6101501@naver.com>
2023-12-20 13:17:30 +0900

    Update vision.cpp
    fuck

--- vision.cpp
+++ vision.cpp
@@ -1,99 +1,20 @@
 #include <iostream>
 #include <stdio.h>
-
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/video/background_segm.hpp>
 
-
-#ifdef _DEBUG        
-#pragma comment(lib, "opencv_core247d.lib")
-#pragma comment(lib, "opencv_imgproc247d.lib")   //MAT processing
-#pragma comment(lib, "opencv_objdetect247d.lib") //HOGDescriptor
-//#pragma comment(lib, "opencv_gpu247d.lib")
-//#pragma comment(lib, "opencv_features2d247d.lib")
-#pragma comment(lib, "opencv_highgui247d.lib")
-#pragma comment(lib, "opencv_ml247d.lib")
-//#pragma comment(lib, "opencv_stitching247d.lib");
-//#pragma comment(lib, "opencv_nonfree247d.lib");
-#pragma comment(lib, "opencv_video247d.lib")
-#else
-#pragma comment(lib, "opencv_core247.lib")
-#pragma comment(lib, "opencv_imgproc247.lib")
-#pragma comment(lib, "opencv_objdetect247.lib")
-//#pragma comment(lib, "opencv_gpu247.lib")
-//#pragma comment(lib, "opencv_features2d247.lib")
-#pragma comment(lib, "opencv_highgui247.lib")
-#pragma comment(lib, "opencv_ml247.lib")
-//#pragma comment(lib, "opencv_stitching247.lib");
-//#pragma comment(lib, "opencv_nonfree247.lib");
-#pragma comment(lib, "opencv_video247d.lib")
-#endif 
-
-
-
+#define TEST
 
 using namespace cv;
 using namespace std;
-#define TEST
 
 #ifdef REAL
-int main(int argc, char* argv[])
-{
-    VideoCapture cap("square.mp4");
-    Mat a, b, c;
-    Mat a_g, b_g, c_g;
-    Mat diff, diff1, diff2;
-    Mat k;
-    int thre = 5;
-
-    Mat frame;
-    Mat Mog2;
-    Ptr<BackgroundSubtractor> pMog2;
-    //pMog2 = createBackgroundSubtractorMOG2();
-    pMOG2 = new BackgroundSubtractorMOG2();
-
-    cap >> a;
-
-    while (1)
-    {
-        cap >> b;
-        cvtColor(a, a_g, COLOR_BGR2GRAY);
-        cvtColor(b, b_g, COLOR_BGR2GRAY);
-
-        absdiff(a_g, b_g, diff1);
-        threshold(diff1, diff1, thre, 255, THRESH_BINARY);
-
-        getStructuringElement(MORPH_RECT, Size(3, 3));
-        morphologyEx(diff1, diff, MORPH_OPEN, k);
-
-        //pMog2->apply(b, frame);
-        pMOG2->operator()(b, frame);
-        morphologyEx(frame, frame, MORPH_ERODE, k);
-        cvtColor(frame, frame, COLOR_GRAY2BGR);
-        bitwise_or(frame, b, diff);
-        imshow("mog2", frame);
-        imshow("diff", diff);
-
-        a = b.clone();
-
-
-        if (waitKey(50) >= 0)
-            break;
-
-    }
-    return 0;
-} 
-#endif
-
-#ifdef TEST
 int main(int argc, char* argv[])
 {
     Mat frame;
     Mat kernel;
     Mat Mog2;
     Ptr<BackgroundSubtractor> pMog2;
+
     //pMog2 = createBackgroundSubtractorMOG2();
     pMog2 = new BackgroundSubtractorMOG2();
 
@@ -107,6 +28,7 @@ int main(int argc, char* argv[])
 
     int hist = 0;
     int total_fix = a.rows * a.cols;
+    int level;
 
     while (1)
     {
@@ -118,15 +40,15 @@ int main(int argc, char* argv[])
         morphologyEx(frame, frame, MORPH_ERODE, kernel);
         
 
-        cvtColor(a, b, COLOR_BGR2GRAY);         //threshold
-        threshold(b, c, 150, 255, THRESH_BINARY);
+        //cvtColor(a, b, COLOR_BGR2GRAY);         //threshold
+        //threshold(b, c, 200, 255, THRESH_BINARY);
         
 
-        bitwise_and(c, frame, c);
+        //bitwise_and(c, frame, c);
 
    
-        imshow("ther", c);
-        findContours(c, contours,RETR_EXTERNAL, CHAIN_APPROX_NONE);
+        imshow("ther", frame);
+        findContours(frame, contours,RETR_EXTERNAL, CHAIN_APPROX_NONE);
         //drawContours(a, contours, -1, Scalar(0, 0, 255), 1);
 
         for (int i = 0; i < contours.size(); i++)
@@ -152,23 +74,120 @@ int main(int argc, char* argv[])
         if (hist >= total_fix * 0.3 && hist < total_fix * 0.4)
         {
             cout << "notice" << endl;
+            level = 2;
         }
         else if (hist >= total_fix * 0.4 && hist < total_fix * 0.5)
         {
             cout << "caution" << endl;
+            level = 4;
         }
         else if (hist >= total_fix * 0.6 && hist < total_fix * 0.7)
         {
             cout << "warning" << endl;
+            level = 6;
         }
-        else if (hist >= total_fix * 0.7 && hist < total_fix * 0.8)
+        else if (hist >= total_fix * 0.7)
         {
             cout << "danger" << endl;
+            level = 8;
         }
 
         hist = 0;
         waitKey(30);
     }
 }
-
 #endif
+
+#ifdef TEST
+int main(int argc, char * argv[])
+{
+    Mat frame;
+    Mat kernel;
+    Mat a, b, c, pre;
+    Mat a_g, b_g, c_g;
+    Mat diff, diff1, diff2;
+    int hist = 0;
+    int total_fix = a.rows * a.cols;
+    int level;
+    
+    //VideoCapture cap("square.mp4");
+    VideoCapture cap(2);
+
+    vector<vector<Point> > contours;
+
+    cap >> a;
+    cap >> b;
+    
+    
+    while (1)
+    {
+        cap >> c;
+        cvtColor(a, a_g, COLOR_BGR2GRAY);
+        cvtColor(b, b_g, COLOR_BGR2GRAY);
+        cvtColor(c, c_g, COLOR_BGR2GRAY);
+        
+        absdiff(a_g, b_g, diff1);
+        threshold(diff1, diff1, 50, 255, THRESH_BINARY);
+        absdiff(b_g, c_g, diff2);
+        threshold(diff2, diff2, 50, 255, THRESH_BINARY);
+
+        bitwise_and(diff1, diff2, diff);
+
+        //drawContours(a, contours, -1, Scalar(0, 0, 255), 1);
+
+
+        kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+        morphologyEx(diff1, diff, MORPH_OPEN, kernel);
+
+
+        imshow("ther", diff);
+
+        findContours(diff, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+        for (int i = 0; i < contours.size(); i++)
+        {
+            Rect rect = boundingRect(contours[i]);
+            rectangle(a, rect, Scalar(0, 0, 255), -1);
+        }
+
+
+        for (int y = 0; y < a.rows; y++)
+        {
+            for (int x = 0; x < a.cols; x++)
+            {
+                if (a.at<Vec3b>(y, x)[2] == 255)
+                {
+                    hist++;
+                }
+            }
+        }
+        
+        imshow("test", a);
+
+        if (hist >= total_fix * 0.3 && hist < total_fix * 0.4)
+        {
+            cout << "notice" << endl;
+            level = 2;
+        }
+        else if (hist >= total_fix * 0.4 && hist < total_fix * 0.5)
+        {
+            cout << "caution" << endl;
+            level = 4;
+        }
+        else if (hist >= total_fix * 0.6 && hist < total_fix * 0.7)
+        {
+            cout << "warning" << endl;
+            level = 6;
+        }
+        else if (hist >= total_fix * 0.7)
+        {
+            cout << "danger" << endl;
+            level = 8;
+        }
+
+        hist = 0;
+        a = b.clone();
+        b = c.clone();
+        waitKey(30);
+    }
+}
+#endif 
